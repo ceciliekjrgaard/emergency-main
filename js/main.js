fe0ec25a0eeef00f609ff54be9b7dceae1975c1a
Cecilie Kjærgaard <cekj0003@stud.ek.dk>
2025-11-21 12:14:23 +0100

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,67 @@
-const form = document.querySelector("form");
+const form = document.querySelector(".form-section");
 
 const firstNameOutput = document.querySelector("#first_name_output");
 const lastNameOutput = document.querySelector("#last_name_output");
 const emailOutput = document.querySelector("#email_output");
 const passwordOutput = document.querySelector("#password_output");
-const langOutput = document.querySelector("#lang_output");
 const termsOutput = document.querySelector("#terms_output");
 
-function cancelPopup(event) {
-  event.preventDefault();
-  form.querySelector(":user-invalid").focus();
+const root = document.querySelector("html");
+const btn = document.querySelector("#toggle");
+
+function toggleDarkMode() {
+  root.classList.toggle("dark");
 }
 
+btn.addEventListener("click", toggleDarkMode);
+
+if (form) {
+  form.addEventListener("invalid", cancelPopup, true);
+  form.addEventListener("submit", handleSubmit);
+}
+function cancelPopup(event) {
+  event.preventDefault();
+}
 function handleSubmit(event) {
   event.preventDefault();
-
-  // 1. Saml værdierne fra formularen
   const formData = new FormData(form);
-  const firstName = formData.get("first_name");
-  const lastName = formData.get("last_name");
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const lang = formData.getAll("language").join(", ");
-  const terms = formData.get("accept_terms");
-
   // 2. Vis værdierne i de rigtige output-felter
+  const firstName = formData.get("first_name");
   firstNameOutput.textContent = firstName;
-  lastNameOutput.textContent = lastName;
-  emailOutput.textContent = email;
-  passwordOutput.textContent = password;
-  langOutput.textContent = lang;
-  termsOutput.textContent = terms;
-
+  lastNameOutput.textContent = formData.get("last_name");
+  emailOutput.textContent = formData.get("email");
+  passwordOutput.textContent = formData.get("password");
+  termsOutput.textContent = formData.get("accept_terms");
   form.reset();
 }
 
-form.addEventListener("invalid", cancelPopup, true);
-form.addEventListener("submit", handleSubmit);
+const book = document.querySelector("#book");
+const wheel = document.querySelector("#wheel");
+const rain = document.querySelector("#rain");
 
-document.getElementById("fornavn").addEventListener("input", function () {
-  document.getElementById("output-fornavn").textContent = this.value;
-});
+const infotext = document.querySelector("#infotext");
+const infobox = document.querySelector("#efficiency");
+const infobox1 = document.querySelector("#requirement");
 
-document.getElementById("efternavn").addEventListener("input", function () {
-  document.getElementById("output-efternavn").textContent = this.value;
-});
+book.addEventListener("click", bookinfo);
+wheel.addEventListener("click", wheelinfo);
+rain.addEventListener("click", raininfo);
 
-document.getElementById("email").addEventListener("input", function () {
-  document.getElementById("output-email").textContent = this.value;
-});
+function bookinfo() {
+  console.log("bookinfo");
+  infotext.textContent = "Studerende";
+  efficiency.textContent = "Unge studerende lever ofter på et stramt budget og en travl kalender og har hverken tid eller overskud til en punkteret cykel";
+  requirement.textContent = "Vi vil gøre det nemt og billigt for studerende at få fikset sin punkteret cykel.";
+}
+function wheelinfo() {
+  console.log("wheelinfo");
+  infotext.textContent = "Punkteret dæk";
+  efficiency.textContent = "Et punkteret dæk er aldrig sjovt. Det kan ske for en hver og hvor som helst";
+  requirement.textContent = "Det skal være nemt og hurtigt at finde hjælp til at få fikset dit punkteret dæk";
+}
+function raininfo() {
+  console.log("raininfo");
+  infotext.textContent = "Det danskevejr";
+  efficiency.textContent = "Vi alle kender det dansek vejr... Det er ikke til at regne med.";
+  requirement.textContent = "Regnen skal ikke stoppe dig, vi hjælper dig til at komme hurtigt i tørvejr";
+}
